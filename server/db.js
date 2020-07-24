eac94f024ead856e0af23eb4971bc40cb5050480
foluso <foluso.akinlade@gmail.com>
2020-07-24 17:02:47 -0400

    db changes

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'Grampsboy',
-	password : 'BallzariaFO69',
-	database : 'nodelogin'
+	host     : 'http://ashmunexpress.com/',
+	user     : 'AshmunExpressApp',
+	password : '$Lincoln1570',
+	database : 'AshmunExpress'
 });
 module.exports = connection
