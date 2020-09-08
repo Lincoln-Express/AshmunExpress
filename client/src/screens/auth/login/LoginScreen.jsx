d8fb5ea2740c98d2c7a86ca0d429e61204bb4e7d
Chidera Iguwe <iguwec@gmail.com>
2020-09-08 15:45:15 -0400

    minor changes

--- client/src/screens/auth/login/LoginScreen.jsx
+++ client/src/screens/auth/login/LoginScreen.jsx
@@ -1,30 +1,30 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-fragments */
-import React, { useContext, useState, Fragment } from "react";
-import { StyleSheet, ImageBackground } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { Formik } from "formik";
-import * as yup from "yup";
-import Logo from "../../../base/Logo/Logo";
-import FilledButton from "../../../base/FilledButton/FilledButton";
-import TextButton from "../../../base/TextButton/TextButton";
-import AuthContext from "../../../contexts/AuthContext";
-import Loading from "../../../base/Loading/Loading";
-import Header from "../../../base/Header/Header";
-import InputField from "../../../base/InputField/InputField";
+import React, { useContext, useState, Fragment } from 'react';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import Logo from '../../../base/Logo/Logo';
+import FilledButton from '../../../base/FilledButton/FilledButton';
+import TextButton from '../../../base/TextButton/TextButton';
+import AuthContext from '../../../contexts/AuthContext';
+import Loading from '../../../base/Loading/Loading';
+import Header from '../../../base/Header/Header';
+import InputField from '../../../base/InputField/InputField';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
+    flexGrow: 1,
+    alignItems: 'center',
     paddingTop: 250,
     padding: 16,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   imageStyle: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     bottom: 0,
@@ -38,75 +38,77 @@ const LoginScreen = () => {
   const { login } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
   const validationSchema = yup.object().shape({
-    email: yup.string().email().required().label("Your input"),
+    email: yup.string().email().required().label('Your input'),
     password: yup
       .string()
       .required()
-      .min(8, "Password should be 8 characters or more")
-      .label("Password"),
+      .min(8, 'Password should be 8 characters or more')
+      .label('Password'),
   });
 
   return (
-    <KeyboardAwareScrollView
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={styles.container}
-      enableOnAndroid
-    >
-      <ImageBackground
-        // eslint-disable-next-line global-require
-        source={require("../../../../assets/background.jpg")}
-        style={styles.imageStyle}
-      />
-
-      <Logo url="../../../../assets/LULogo.png" />
-      <Header>Welcome</Header>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            await login(values.email, values.password);
-            // setLoading(true);
-          } catch (e) {
-            setLoading(false);
-          }
-        }}
-        validationSchema={validationSchema}
+    <Fragment>
+      <KeyboardAwareScrollView
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        contentContainerStyle={styles.container}
+        enableOnAndroid
+        automaticallyAdjustContentInsets
       >
-        {(formikProps) => (
-          <Fragment>
-            <InputField
-              label="Email"
-              formikProps={formikProps}
-              pointer="email"
-              placeholder="johndoe@email.com"
-              placeholderTextColor="#808080"
-              autoFocus
-            />
-            <InputField
-              label="Password"
-              formikProps={formikProps}
-              pointer="password"
-              placeholder="********"
-              placeholderTextColor="#808080"
-              secureTextEntry
-              autoFocus
-            />
+        <ImageBackground
+          // eslint-disable-next-line global-require
+          source={require('../../../../assets/background.jpg')}
+          style={styles.imageStyle}
+        />
+        <Logo url='../../../../assets/LULogo.png' />
+        <Header>Welcome</Header>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={async (values) => {
+            try {
+              await login(values.email, values.password);
+              // setLoading(true);
+            } catch (e) {
+              setLoading(false);
+            }
+          }}
+          validationSchema={validationSchema}
+        >
+          {(formikProps) => (
+            <Fragment>
+              <InputField
+                label='Email'
+                formikProps={formikProps}
+                pointer='email'
+                placeholder='johndoe@email.com'
+                placeholderTextColor='#808080'
+                autoFocus
+              />
+              <InputField
+                label='Password'
+                formikProps={formikProps}
+                pointer='password'
+                placeholder='********'
+                placeholderTextColor='#808080'
+                secureTextEntry
+                autoFocus
+              />
 
-            <FilledButton
-              title="Login"
-              handlePress={formikProps.handleSubmit}
-            />
-            <TextButton
-              title={"Don't have an account? create one here"}
-              handlePress={() => {
-                navigation.navigate("Registration");
-              }}
-            />
-          </Fragment>
-        )}
-      </Formik>
-      <Loading loading={loading} />
-    </KeyboardAwareScrollView>
+              <FilledButton
+                title='Login'
+                handlePress={formikProps.handleSubmit}
+              />
+              <TextButton
+                title={"Don't have an account? create one here"}
+                handlePress={() => {
+                  navigation.navigate('Registration');
+                }}
+              />
+            </Fragment>
+          )}
+        </Formik>
+        <Loading loading={loading} />
+      </KeyboardAwareScrollView>
+    </Fragment>
   );
 };
 
